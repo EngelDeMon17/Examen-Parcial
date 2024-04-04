c665d1e18270377687f4a7e6d328455b075740f9
Waldemar17@umesoamericana.onmicrosoft.com <Waldemar17@umesoamericana.onmicrosoft.com>
2024-04-04 08:09:12 -0600

    creacion de clases y listas

---
+++ Inscripciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Parcial
+{
+    internal class Inscripciones
+    {
+        int dpiEstudiante;
+        string codigoTaller;
+        DateTime fechaInscripcion;
+
+        public int DpiEstudiante { get => dpiEstudiante; set => dpiEstudiante = value; }
+        public string CodigoTaller { get => codigoTaller; set => codigoTaller = value; }
+        public DateTime FechaInscripcion { get => fechaInscripcion; set => fechaInscripcion = value; }
+    }
+}
---
+++ Talleres.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Parcial
+{
+    internal class Talleres
+    {
+        string codigoTaller;
+        string nombreTaller;
+        string costoTaller;
+
+        public string CodigoTaller { get => codigoTaller; set => codigoTaller = value; }
+        public string NombreTaller { get => nombreTaller; set => nombreTaller = value; }
+        public string CostoTaller { get => costoTaller; set => costoTaller = value; }
+    }
+}
